4e1b6ccc9a44a2317d9a0c5d0ed693ce21c70b6c
zildevvn <vuongdevvn0509@gmail.com>
2026-04-05 14:15:57 +0700

    add company page

--- company-template.php
+++ company-template.php
@@ -5,10 +5,10 @@
 get_header();
 ?>
 
-    <main id="primary" class="site-main">
-        <?php get_template_part('template-parts/company/hero-section'); ?>
-        <?php get_template_part('template-parts/company/access-section'); ?>
-        <?php get_template_part('template-parts/company/rinen-section'); ?>
-        <?php get_template_part('template-parts/company/message-section'); ?>
-    </main>
+<main id="primary" class="site-main">
+    <?php get_template_part('template-parts/company/hero-section'); ?>
+    <?php get_template_part('template-parts/company/breadcrumbs-section'); ?>
+    <?php get_template_part('template-parts/company/content-section'); ?>
+    <?php get_template_part('template-parts/shared/contact-section'); ?>
+</main>
 <?php get_footer(); ?>
